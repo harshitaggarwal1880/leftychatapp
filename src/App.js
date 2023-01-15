8118a183e5823ffd9215a3fb79c2b0f479e6e649
Harsh <harsh@gmail.com>
2023-01-15 15:20:35 +0530

    Adding lazy loading code splitting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Chat from './Pages/Chat'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import SetAvatar from './Pages/SetAvatar'
+// import Chat from './Pages/Chat'
+// import Login from './Pages/Login'
+// import Register from './Pages/Register'
+// import SetAvatar from './Pages/SetAvatar'
 
+const Chat = lazy(()=> import('./Pages/Chat'))
+const Login = lazy(()=> import('./Pages/Login'))
+const Register = lazy(()=> import('./Pages/Register'))
+const SetAvatar = lazy(()=> import('./Pages/SetAvatar'))
 
 const App = () => {
   return (
