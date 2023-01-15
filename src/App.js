819b56ba0622bcf3c4af75a4a0787c98884b1935
Harsh <harsh@gmail.com>
2023-01-15 16:42:21 +0530

    Code splitting added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Chat from './Pages/Chat'
 // import Login from './Pages/Login'
 // import Register from './Pages/Register'
 // import SetAvatar from './Pages/SetAvatar'
 
-const Chat = lazy(()=> import('./Pages/Chat'))
-const Login = lazy(()=> import('./Pages/Login'))
-const Register = lazy(()=> import('./Pages/Register'))
-const SetAvatar = lazy(()=> import('./Pages/SetAvatar'))
+const Chat = lazy(() => import("./Pages/Chat"));
+const Login = lazy(() => import("./Pages/Login"));
+const Register = lazy(() => import("./Pages/Register"));
+const SetAvatar = lazy(() => import("./Pages/SetAvatar"));
 
 const App = () => {
   return (
-    <Router> 
+    <Router>
       <Suspense fallback={<div>Loading...</div>}>
-      <Routes> 
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/" element={<Chat/>} />
-        <Route path="/setavatar" element={<SetAvatar/>} />
-      </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Chat />} />
+          <Route path="/setavatar" element={<SetAvatar />} />
+        </Routes>
       </Suspense>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
